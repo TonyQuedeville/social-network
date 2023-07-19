bf05cbbde8508fe1f6b74e4996b33dd5a77047a5
Alann <AlannSchnebelen@hotmail.com>
2023-07-19 10:01:39 +0200

    add status users

--- database-manager/structs/user/methode.go
+++ database-manager/structs/user/methode.go
@@ -71,7 +71,7 @@ func GetUsers(user_id uint64) []*User {
 		u.id,
 		u.pseudo,
 		u.image,
-		%v,
+		u.status,
 		%v,
 		%v,
 		%v,
@@ -79,14 +79,14 @@ func GetUsers(user_id uint64) []*User {
 		%v
 	FROM user u
 	LEFT JOIN follower f ON f.user_id = u.id AND f.follow_id = %v;
-	`, fmt.Sprintf(c, user_id, "about", "about"), fmt.Sprintf(c, user_id, "sexe", "sexe"), fmt.Sprintf(c, user_id, "first_name", "first_name"), fmt.Sprintf(c, user_id, "last_name", "last_name"), fmt.Sprintf(c, user_id, "date_of_birth", "date_of_birth"), fmt.Sprintf(c, user_id, "status", "status"), user_id))
+	`, fmt.Sprintf(c, user_id, "about", "about"), fmt.Sprintf(c, user_id, "sexe", "sexe"), fmt.Sprintf(c, user_id, "first_name", "first_name"), fmt.Sprintf(c, user_id, "last_name", "last_name"), fmt.Sprintf(c, user_id, "date_of_birth", "date_of_birth"), user_id))
 	if err != nil {
 		fmt.Println("ERREUR QUERY ALL USERS: ", err)
 	}
 	result := []*User{}
 	for rows.Next() {
 		u := &User{}
-		rows.Scan(&u.Id, &u.Pseudo, &u.Image, &u.About, &u.Sexe, &u.First_name, &u.Last_name, &u.Born_date, &u.Status)
+		rows.Scan(&u.Id, &u.Pseudo, &u.Image, &u.Status, &u.About, &u.Sexe, &u.First_name, &u.Last_name, &u.Born_date)
 		fmt.Printf("u: %v\n", u)
 		result = append(result, u)
 	}
