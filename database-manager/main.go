84b0b103f4d2c481419b92769fad6cf01787e5be
Alann <AlannSchnebelen@hotmail.com>
2023-08-21 11:07:43 +0200

    corection event info vide

--- database-manager/main.go
+++ database-manager/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/TonyQuedeville/social-network/database-manager/database"
+	event "github.com/TonyQuedeville/social-network/database-manager/structs/event_"
 )
 
 // no program here ;)
@@ -9,6 +12,7 @@ func main() {
 	// test
 	database.OpenDatabase()
 	defer database.CloseDatabase()
+	fmt.Printf("event.ReadEventByGroupId(1): %v\n", event.CheckNewEvent(1)[0])
 	// e := event.Event{
 	// 	Group_id:    1,
 	// 	Titre:       "TitreEvent",
