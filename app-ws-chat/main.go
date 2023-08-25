81e26ea32f89cb0730cac47d4aced03a65ca03a2
Alann <AlannSchnebelen@hotmail.com>
2023-08-25 09:59:25 +0200

    api ws test connection

--- app-ws-chat/main.go
+++ app-ws-chat/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/TonyQuedeville/social-network/app-ws-chat/pkg/server"
 	"github.com/TonyQuedeville/social-network/database-manager/database"
 )
 
 func main() {
 	database.OpenDatabase()
 	defer database.CloseDatabase()
+	server.StartServer
 	ch := make(chan struct{})
 	<-ch
 }
