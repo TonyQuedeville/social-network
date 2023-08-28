de5d3d8efda8969cdde288cab4aede711bf3b5ae
Alann <AlannSchnebelen@hotmail.com>
2023-08-28 15:15:20 +0200

    le ws marche !!!!

--- app-ws-chat/go.mod
+++ app-ws-chat/go.mod
@@ -2,9 +2,11 @@ module github.com/TonyQuedeville/social-network/app-ws-chat
 
 go 1.19
 
+require github.com/googollee/go-socket.io v1.7.0
+
 require (
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/gomodule/redigo v1.8.4 // indirect
-	github.com/googollee/go-socket.io v1.7.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
 )
--- app-ws-chat/pkg/server/ws.go
+++ app-ws-chat/pkg/server/ws.go
@@ -1,17 +1,64 @@
 package server
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
 	socketio "github.com/googollee/go-socket.io"
 )
 
-func StartServer() {
+func Test() {
 	server := socketio.NewServer(nil)
+
+	// Configurer les en-têtes CORS
 	server.OnConnect("/", func(s socketio.Conn) error {
+		log.Println("Client connected:", s.ID())
+		// Autoriser toutes les origines
 		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		s.Emit("hello", "world")
 		return nil
+	})
+
+	server.OnEvent("/", "message", func(s socketio.Conn, msg string) {
+		log.Println("Message received:", msg)
+		// Vous pouvez émettre une réponse au client ici si nécessaire
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println("Client disconnected:", s.ID())
+	})
+
+	http.Handle("/socket.io/", cors(server))
+	// http.Handle("/", cors(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// })))
+	log.Println("Server is running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Autoriser toutes les origines
+		// w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5501")
+		// w.Header().Set("Access-Control-Allow-Credentials", "true")
+		// w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		// w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
+		// if r.Method == "OPTIONS" {
+		// 	w.WriteHeader(http.StatusOK)
+		// 	return
+		// }
+		// next.ServeHTTP(w, r)*
+		// If it's a preflight request, just return without calling the next handler
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Content-Type", "text/plain")
+			w.Header().Set("Access-Control-Allow-Origin", "*") // A CHANGER POUR UN POTENTIEL DEPLOIMENT
+			w.Header().Set("Access-Control-Allow-Credentials", "false")
+			return
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // A CHANGER POUR UN POTENTIEL DEPLOIMENT
+
+		// Allow the use of credentials (like cookies) in the requests
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		next.ServeHTTP(w, r)
 	})
 }
